5028702448e810d6acacb0ee737266e479e15c48
Julien Calixte <julienc@bam.tech>
2019-10-17 22:04:03 +0200

    :heavy_plus_sign: (Material UI)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
