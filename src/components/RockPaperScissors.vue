16eb35ec67899410c57591d0d300aaefe3af202b
Julien Calixte <julienc@bam.tech>
2019-10-17 21:43:34 +0200

    :tada:

---
+++ src/components/RockPaperScissors.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rock-paper-scissors">
+    <h1>Play</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "RockPaperScissors",
+  props: {
+    id: String
+  }
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RockPaperScissors id="" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import RockPaperScissors from "@/components/RockPaperScissors.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    RockPaperScissors
   }
 };
 </script>
